8b6e4b97c2a13693f58437914ba3ae1fcc633f44
ArsLoD <arslod123@gmail.com>
2022-08-27 18:09:38 +0300

    upd2

--- src/components/pages/people-page.js
+++ src/components/pages/people-page.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { PersonDetails, PersonList } from '../sw-components';
-import Row from '../row';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { PersonDetails, PersonList } from "../sw-components";
+import Row from "../row";
 
 const PeoplePage = ({ history, match }) => {
-
   const { id } = match.params;
 
   return (
-    <Row
-      left={<PersonList onItemSelected={(id) => history.push(id)} />}
-      right={<PersonDetails itemId={id} />} />
+    <>
+      <Row
+        left={<PersonList onItemSelected={(id) => history.push(id)} />}
+        right={<PersonDetails itemId={id} />}
+      />
+      <Row left={<img src="./artur.jpg" />} right={<img src="./bog.jpg" />} />
+      <h1>Иди нахуй со своим плутоном</h1>
+    </>
   );
 };
 
