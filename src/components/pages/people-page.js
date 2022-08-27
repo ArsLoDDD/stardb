664ac347579cd3e237b6f944c7b61df4693778cc
ArsLoD <arslod123@gmail.com>
2022-08-27 18:16:25 +0300

    lst upd

--- src/components/pages/people-page.js
+++ src/components/pages/people-page.js
@@ -7,14 +7,21 @@ const PeoplePage = ({ history, match }) => {
   const { id } = match.params;
 
   return (
-    <>
+    <div>
       <Row
         left={<PersonList onItemSelected={(id) => history.push(id)} />}
         right={<PersonDetails itemId={id} />}
       />
-      <Row left={<img src="./artur.jpg" />} right={<img src="./bog.jpg" />} />
+      <Row
+        left={
+          <img src="https://i.ibb.co/SNC0TxR/photo-5210967246690696845-y.jpg" />
+        }
+        right={
+          <img src="https://i.ibb.co/5Ln5zkD/photo-5210967246690696844-y.jpg" />
+        }
+      />
       <h1>Иди нахуй со своим плутоном</h1>
-    </>
+    </div>
   );
 };
 
